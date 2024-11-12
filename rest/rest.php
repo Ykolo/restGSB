5a9e75a11f375cc8be142490abf4edeb14c8dcae
Ykolo <72657098+Ykolo@users.noreply.github.com>
2024-11-12 15:42:24 +0100

    Update rest.php

--- rest/rest.php
+++ rest/rest.php
@@ -109,9 +109,7 @@ abstract class Rest {
             }
         }
         else {
-            if (get_magic_quotes_gpc()) {
-                $data = trim(stripslashes($data));
-            }
+            $data = trim(stripslashes($data));
             $data = strip_tags($data);
             $clean_input = trim($data);
         }
